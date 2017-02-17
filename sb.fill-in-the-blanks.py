8963ff179865725da1904d10a659ddff57f6fdab
shawnbennett <shawnjbennett@gmail.com>
2017-02-16 19:35:06 -0500

    adding in my notes as I begin the project

---
+++ sb.fill-in-the-blanks.py
@@ -0,0 +1,80 @@
+# IPND Stage 2 Final Project
+
+# You've built a Mad-Libs game with some help from Sean.
+# Now you'll work on your own game to practice your skills and demonstrate what you've learned.
+
+# For this project, you'll be building a Fill-in-the-Blanks quiz.
+# Your quiz will prompt a user with a paragraph containing several blanks.
+# The user should then be asked to fill in each blank appropriately to complete the paragraph.
+# This can be used as a study tool to help you remember important vocabulary!
+
+# Note: Your game will have to accept user input so, like the Mad Libs generator,
+# you won't be able to run it using Sublime's `Build` feature.
+# Instead you'll need to run the program in Terminal or IDLE.
+# Refer to Work Session 5 if you need a refresher on how to do this.
+
+# To help you get started, we've provided a sample paragraph that you can use when testing your code.
+# Your game should consist of 3 or more levels, so you should add your own paragraphs as well!
+
+sample = '''A ___1___ is created with the def keyword. You specify the inputs a ___1___ takes by
+adding ___2___ separated by commas between the parentheses. ___1___s by default return ___3___ if you
+don't specify the value to return. ___2___ can be standard data types such as string, number, dictionary,
+tuple, and ___4___ or can be more complicated such as objects and lambda functions.'''
+
+# The answer for ___1___ is 'function'. Can you figure out the others?
+
+# We've also given you a file called fill-in-the-blanks.pyc which is a working version of the project.
+# A .pyc file is a Python file that has been translated into "byte code".
+# This means the code will run the same as the original .py file, but when you open it
+# it won't look like Python code! But you can run it just like a regular Python file
+# to see how your code should behave.
+
+# Hint: It might help to think about how this project relates to the Mad Libs generator you built with Sean.
+# In the Mad Libs generator, you take a paragraph and replace all instances of NOUN and VERB.
+# How can you adapt that design to work with numbered blanks?
+
+# If you need help, you can sign up for a 1 on 1 coaching appointment: https://calendly.com/ipnd-1-1/20min/
+
+
+#what does this project have to do?
+
+	# 1. open and ask 'Who do I have the pleaseure of speaking with today? - assign this as NAME 
+		# respond with "Hello NAME would you like to play a game?" provide the response options 'yes/no'
+		# if yes - then - 'Excellent, I was hoping you would say that. Lets begin'
+		# if no - then  - 'Did you really think you could get out of this? Let's begin!
+	# 2. the next thing to do is to ask "Do you feel lucky? Well, do ya?" and provide the user with three options to select from
+		# Show "Please place your Quiz order" - "Tall", "Grande", or "Venti"
+		# Tall Quiz - """
+		# Grande Quiz - 
+		# Venti Quiz - 
+
+		#The proper Quiz will need to display with 1-4 listed as blanks
+			#if the first answer is correct then print the quiz with that answer filled in and ask for the next response...repeat until complete.
+			#if all answers are correct then give some kind of congrats message
+			#when a wrong response is given then have some kind of wrong response and end the quiz
+
+
+
+
+
+
+
+#what are the inputs that I need?
+	#Users NAME
+
+	#Tall quiz - 
+	#Grande quiz
+	#Venti quiz
+	#Tall answers  ___1___  ___2___  ___3___  ___4___
+	#Grande answers ___1___  ___2___  ___3___  ___4___
+	#Venti answers ___1___  ___2___  ___3___  ___4___
+
+
+
+
+
+
+
+#what are the outputs that I need?
+
+
